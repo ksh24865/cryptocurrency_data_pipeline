01287b126c441b92876d11217d56f901d82464a6
ksh24865 <8819889@naver.com>
2022-03-27 19:54:28 +0900

    [FEAT] Add CryptocurrencyPriceSourcingBatchHourlyOperator

--- dags/operators/cryptocurrency/price/sourcing_batch_hourly.py
+++ dags/operators/cryptocurrency/price/sourcing_batch_hourly.py
@@ -13,18 +13,17 @@ from utils.date import (
 )
 
 
-class CryptocurrencyPriceSourcingBatchDailyOperator(
+class CryptocurrencyPriceSourcingBatchHourlyOperator(
     CryptocurrencyPriceSourcingBatchOperator
 ):
     YEARS_BEFORE_FOR_CRYPTOCURRENCY_PRICE_DAILY: Final[int] = 7
 
     @property
     def api_endpoint(self):
-        return "histoday"
+        return "histohour"
 
     def timestamp_generator(self) -> Iterable[Tuple[int, timedelta]]:
         execution_date = str_to_date(self.execution_date)
-        print(f"execution_date: {execution_date}")
         for start_date, end_date in date_range(
             start_date=CRYPTO_COMPARE_OLDEST_DATE,
             end_date=get_date_years_before(
